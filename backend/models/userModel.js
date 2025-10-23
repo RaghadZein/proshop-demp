65baeaa76e7746e88e7b1d43bbe3c8df2f834eaf
RaghadZein <raghdzeinn@gmail.com>
2025-10-23 23:35:26 +0300

    schema and models

---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const userSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, default: false, required: true },
+  },
+  { timestamps: true }
+);
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return enteredPassword === this.password;
+};
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  } else {
+    this.password = await hashPassword(this.password);
+    next();
+  }
+});
+const User = mongoose.model("User", userSchema);
+export default User;
